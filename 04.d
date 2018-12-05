b7f1eea9af01a164bc632c93721bbf379504c6c7
Yoav Shai <gergesh@gmail.com>
2018-12-05 20:41:00 +0200

    Clean up day 4

--- 04.d
+++ 04.d
@@ -5,40 +5,48 @@ import std.file : readText, FileException;
 import std.string : splitLines, indexOf;
 import std.algorithm.sorting : sort;
 import std.conv : to;
-import std.algorithm.searching : maxIndex, maxElement;
-
-struct Nap {
-    int start;
-    int end;
-}
 
 struct Guard {
-    int totalsleep = 0;
-    Nap[] naps;
+    int total_sleep = 0;
+    int[60] sleep_per_minute;
+}
+
+int sleepiestMin(Guard g) {
+    int val = 0, ind;
+    foreach (min; 0 .. 60) {
+        if (g.sleep_per_minute[min] > val) {
+            val = g.sleep_per_minute[min];
+            ind = min;
+        }
+    }
+
+    return ind;
 }
 
 Guard[int] parseGuards(string[] lines) {
     Guard[int] guards;
-    sort(lines);
     int gid, start, end;
+
+    sort(lines);
     foreach (line; lines) {
         final switch (line[19]) {
             case 'G': // Guard begins shift
                 gid = to!int(line[26 .. indexOf(line, " ", 26)]);
-                if (!(gid in guards)) {
+                if (!(gid in guards))
                     guards[gid] = Guard();
-                }
                 break;
             case 'f': // falls asleep
                 start = to!int(line[15 .. 17]);
                 break;
             case 'w': // wakes up
                 end = to!int(line[15 .. 17]);
-                guards[gid].naps ~= Nap(start, end);
-                guards[gid].totalsleep += end - start;
+                guards[gid].total_sleep += end - start;
+                while (end-- != start)
+                    guards[gid].sleep_per_minute[end]++;
                 break;
         }
     }
+
     return guards;
 }
 
@@ -57,38 +65,27 @@ int main(string[] args) {
     }
 
     Guard[int] guards = parseGuards(lines);
-    int max = 0, maxID;
+
+    int currGuardSleepiestMinute;
+    int p1gid, p1gtm, p1ghs = 0;
+    int p2gid, p2gtm, p2ghs = 0;
     foreach (id; guards.keys) {
-        if (guards[id].totalsleep > max) {
-            max = guards[id].totalsleep;
-            maxID = id;
+        currGuardSleepiestMinute = sleepiestMin(guards[id]);
+
+        if (guards[id].total_sleep > p1ghs) {
+            p1gid = id;
+            p1gtm = currGuardSleepiestMinute;
+            p1ghs = guards[id].total_sleep;
+        }
+
+        if (guards[id].sleep_per_minute[currGuardSleepiestMinute] > p2ghs) {
+            p2gid = id;
+            p2gtm = currGuardSleepiestMinute;
+            p2ghs = guards[id].sleep_per_minute[currGuardSleepiestMinute];
         }
     }
 
-    int[] napTotal;
-    foreach (_; 0..60) {napTotal ~= 0;}
-    foreach (nap; guards[maxID].naps) {
-        foreach(min; nap.start..nap.end) {
-            napTotal[min]++;
-        }
-    }
-
-    writeln(napTotal.maxIndex * maxID);
-
-    long maxMinutes = 0, maxMinute;
-    foreach(id; guards.keys) {
-        foreach (x; 0..60) {napTotal[x] = 0;}
-        foreach (nap; guards[id].naps) {
-            foreach(min; nap.start..nap.end) {
-                napTotal[min]++;
-            }
-        }
-        if (napTotal.maxElement > maxMinutes) {
-            maxMinutes = napTotal.maxElement;
-            maxID = id;
-            maxMinute = napTotal.maxIndex;
-        }
-    }
-    writeln(maxID * maxMinute);
+    writeln("Part 1: ", p1gid * p1gtm);
+    writeln("Part 2: ", p2gid * p2gtm);
     return 0;
 }
